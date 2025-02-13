eaf8807256a7ecbaede88dbde816a69b82f36cf0
Sean Horvath <sean.horvath@noaa.gov>
2025-02-13 00:03:45 +0000

    fix setting default reservoir DA parameters when there are no waterbodies

--- src/troute-network/troute/HYFeaturesNetwork.py
+++ src/troute-network/troute/HYFeaturesNetwork.py
@@ -630,11 +630,16 @@ class HYFeaturesNetwork(AbstractNetwork):
             self._waterbody_type_specified = True
             
         else:
-
-            self.data_assimilation_parameters['reservoir_da']['reservoir_persistence_da']['reservoir_persistence_usgs'] = False
-            self.data_assimilation_parameters['reservoir_da']['reservoir_persistence_da']['reservoir_persistence_usace'] = False
-            self.data_assimilation_parameters['reservoir_da']['reservoir_persistence_da']['reservoir_persistence_canada'] = False
-            self.data_assimilation_parameters['reservoir_da']['reservoir_rfc_da']['reservoir_rfc_forecasts'] = False
+            self.data_assimilation_parameters['reservoir_da'] = {
+                'reservoir_persistence_da': {
+                    'reservoir_persistence_usgs': False,
+                    'reservoir_persistence_usace': False,
+                    'reservoir_persistence_canada': False,
+                    },
+                'reservoir_rfc_da': {
+                    'reservoir_rfc_forecasts': False
+                    }
+                }
             self.waterbody_parameters['break_network_at_waterbodies'] = False
 
             self._waterbody_df = pd.DataFrame()
